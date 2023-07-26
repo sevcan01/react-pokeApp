e187363d3300f395a2190cf86362ac9c2bb3b3f7
sevcan01 <swcnatcn@gmail.com>
2023-07-26 22:40:11 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import './Pokecard.css'
+import Pokedex from './Pokegame'
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Pokemon Go Card Game</h1>
+      <Pokedex />
+    </div>
+  );
+}
+
+export default App;
